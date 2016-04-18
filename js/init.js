6483783df10080f37bfeec4bc87c496618b734a1
irvin373 <irvin373@gmail.com>
2016-04-17 20:35:45 -0400

    primera vista

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+(function($){
+  $(function(){
+    $(document).ready(function(){
+      $('.parallax').parallax();
+  });
+  }); // end of document ready
+})(jQuery);
